60e60834e927d8549859331c70e435876b3b46f0
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-11 22:49:08 +0300

    Scriptable Object

---
+++ Tutorial_2_The_Flyweight_Pattern/Assets/Scripts/CreateWorldCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWorldCube : MonoBehaviour
+{
+    public int width;
+    public int depth;
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+            for (int z = 0; z < depth; z++)
+            {
+                Vector3 pos = new Vector3(x, Mathf.PerlinNoise(x*.5f,z*.5f), z);
+                GameObject go = Instantiate(cube,pos,Quaternion.identity);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tutorial_2_The_Flyweight_Pattern/Assets/Scripts/Plant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField] private PlantData info;
+    private SetPlantInfo spi;
+
+    private void Start()
+    {
+        spi = GameObject.FindWithTag("PlantInfo").GetComponent<SetPlantInfo>();
+    }
+
+    private void OnMouseDown()
+    {
+        spi.OpenPlantPanel();
+        spi.plantName.text = info.Name;
+        spi.threatLevel.text = info.Threat.ToString();
+        spi.plantIcon.GetComponent<RawImage>().texture = info.Icon;
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag=="Player" && info.Threat == PlantData.THREAT.High)
+        {
+            PlayerController.dead = true;
+        }
+    }
+}
