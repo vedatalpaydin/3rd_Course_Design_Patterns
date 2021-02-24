1e0c9a3c863dbddd026810010e525b86e65b70a9
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-24 13:24:45 +0300

    The Flyweight Pattern

---
+++ Tutorial_2_The_Flyweight_Pattern/Assets/Warthog/SpawnProxy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections; 
+using System.Collections.Generic;
+using System;
+using Unity.Entities;
+using UnityEngine;
+
+public class SpawnProxy : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+{
+    public GameObject cube;
+    public int rows;
+    public int cols;
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(cube);
+    }
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var spawnerData = new Spawner
+        {
+            Prefab = conversionSystem.GetPrimaryEntity(cube),
+            Erows = rows,
+            Ecols = cols 
+        };
+        dstManager.AddComponentData(entity, spawnerData);
+    }
+}
---
+++ Tutorial_2_The_Flyweight_Pattern/Assets/Warthog/Spawner.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+public class Spawner : IComponentData
+{
+   public Entity Prefab;
+   public int Erows;
+   public int Ecols;
+}
