3071918bf0eb133020e8327f7663152fcd4ce2e0
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-24 14:21:02 +0300

    Observers Part 2

---
+++ Tutotial_3_The_Observer_Pattern/Assets/Radar/Event.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Event",menuName = "Game Event", order = 52)]
+public class Event : ScriptableObject
+{
+    private List<EventListener> eListeners = new List<EventListener>();
+
+    public void Register(EventListener listener)
+    {
+        eListeners.Add(listener);
+    }
+
+    public void UnRegister(EventListener listener)
+    {
+        eListeners.Remove(listener);
+    }
+
+    public void Occured()
+    {
+        for (int i = 0; i < eListeners.Count; i++)
+        {
+            eListeners[i].OnEventOccurs();
+        }
+    }
+}
---
+++ Tutotial_3_The_Observer_Pattern/Assets/Radar/EventListener.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventListener : MonoBehaviour
+{
+    public Event gEvent;
+    public UnityEvent response;
+
+    private void OnEnable()
+    {
+        gEvent.Register(this);
+    }
+
+    private void OnDisable()
+    {
+        gEvent.UnRegister(this);
+    }
+
+    public void OnEventOccurs()
+    {
+        response.Invoke();
+    }
+}
