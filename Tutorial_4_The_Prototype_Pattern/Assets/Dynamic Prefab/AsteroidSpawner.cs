84c1dd33b95971932b50fe045560981e7ad46f66
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-24 22:50:26 +0300

    Dynamic Prefab Creation

---
+++ Tutorial_4_The_Prototype_Pattern/Assets/Dynamic Prefab/AsteroidSpawner.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public Material material;
+    public GameObject asteroid;
+
+
+    public void CreateAsteroid()
+    {
+        asteroid = ProcAsteroid.Clone(transform.position);
+        asteroid.GetComponent<MeshRenderer>().sharedMaterial = material;
+    }
+    
+}
---
+++ Tutorial_4_The_Prototype_Pattern/Assets/Dynamic Prefab/Editor/AsteroidSpawnEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AsteroidSpawner))]
+public class AsteroidSpawnEditor : Editor
+{
+    private string path;
+    private string assetpath;
+    private string filename;
+
+    private void OnEnable()
+    {
+        path = Application.dataPath + "/Asteroid";
+        assetpath = "Assets/Asteroid";
+        filename = "asteroid_" + System.DateTime.Now.Ticks.ToString();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AsteroidSpawner astSpawner = (AsteroidSpawner) target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Create Asteroid"))
+            astSpawner.CreateAsteroid();
+        if (GUILayout.Button("Save Asteroid"))
+        {
+            System.IO.Directory.CreateDirectory(path);
+            Mesh mesh = astSpawner.asteroid.GetComponent<MeshFilter>().sharedMesh;
+            AssetDatabase.CreateAsset(mesh,assetpath+mesh.name+ ".asset");
+            AssetDatabase.SaveAssets();
+
+            PrefabUtility.SaveAsPrefabAsset(astSpawner.asteroid, assetpath + filename + ".prefab");
+        }
+    }
+}
