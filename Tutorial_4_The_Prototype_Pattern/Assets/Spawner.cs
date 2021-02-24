82041af749ce722b418ce2c5952e201a9e60f5ec
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-24 20:17:29 +0300

    The Prototype Pattern

---
+++ Tutorial_4_The_Prototype_Pattern/Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject spherePrefab;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+            Instantiate(cubePrefab, transform.position, Quaternion.identity);
+        else if (Random.Range(0, 100) < 10)
+            Instantiate(spherePrefab, transform.position, Quaternion.identity);
+    }
+}
