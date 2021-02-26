34865c70157dc5695aacbd1b215344e3f7772945
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-26 16:34:10 +0300

    Setting Up The Project

---
+++ Tutorial_7_The_Object_Pool/Assets/Move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 velocity;
+  
+    void Update()
+    {
+        transform.Translate(velocity);
+    }
+}
---
+++ Tutorial_7_The_Object_Pool/Assets/Spawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject asteroid;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 5)
+            Instantiate(asteroid, transform.position + new Vector3(Random.Range(-10, 10), 0, 0), Quaternion.identity);
+    }
+}
