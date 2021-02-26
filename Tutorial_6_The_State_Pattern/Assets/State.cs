7c5b7d6c0cd89fc09100f43135e9c8b9d0b5710b
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-26 08:53:59 +0300

    Creating and Using State Class

---
+++ Tutorial_6_The_State_Pattern/Assets/State.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State
+{
+    public enum STATE
+    {
+        IDLE,
+        PATROL,
+        PURSUE,
+        ATTACK,
+        SLEEP
+    };
+
+    public enum EVENT
+    {
+        ENTER,
+        UPDATE,
+        EXIT
+    };
+
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+    protected NavMeshAgent agent;
+
+    private float visDist = 10.0f;
+    private float visAngle = 30.0f;
+    private float shootDist = 7.0f;
+
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        agent = _agent;
+        anim = _anim;
+        stage = EVENT.ENTER;
+        player = _player;
+    }
+
+    public virtual void Enter()
+    {
+        stage = EVENT.UPDATE;
+    }
+
+    public virtual void Update()
+    {
+        stage = EVENT.UPDATE;
+    }
+
+    public virtual void Exit()
+    {
+        stage = EVENT.EXIT;
+    }
+
+    public State Process()
+    {
+        if (stage == EVENT.ENTER) Enter();
+        if (stage == EVENT.UPDATE) Update();
+        if (stage == EVENT.EXIT)
+        {
+            Exit();
+            return nextState;
+        }
+
+        return this;
+    }
+}
+
+public class Idle : State
+{
+    public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isIdle");
+        base.Exit();
+    }
+}
